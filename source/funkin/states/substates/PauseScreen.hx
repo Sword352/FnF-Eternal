5fe0c982901cd6ae4bbc0529b9d7d79c1ed82b48
Sword352 <sword352email@gmail.com>
2023-12-02 09:13:35 +0100

    pause substate rewrite (kinda)

--- source/funkin/states/substates/PauseScreen.hx
+++ source/funkin/states/substates/PauseScreen.hx
@@ -13,7 +13,12 @@ import funkin.states.menus.FreeplayMenu;
 import funkin.states.options.OptionsMenu;
 
 class PauseScreen extends MusicBeatSubState {
-    var list:Array<String> = ["Resume", "Restart Song", "Go To Options", "Exit To Menu"];
+    // in case i do difficulty stuff later
+    var itemLists:Map<String, Array<String>> = [
+        "default" => ["Resume", "Restart Song", "Go To Options", "Exit To Menu"]
+    ];
+
+    var currentList:Array<String>;
     var currentSelection:Int = 0;
 
     var background:FlxSprite;
@@ -51,12 +56,7 @@ class PauseScreen extends MusicBeatSubState {
         items = new FlxTypedGroup<Alphabet>();
         add(items);
 
-        for (i in 0...list.length) {
-            var item = new Alphabet(0, 0, list[i]);
-            item.menuItem = true;
-            item.ID = i;
-            items.add(item);
-        }
+        changeList("default");
 
         songText = new FlxText(0, 15);
         songText.setFormat(AssetHelper.font('vcr'), 32);
@@ -74,14 +74,12 @@ class PauseScreen extends MusicBeatSubState {
         lossCounter.alpha = 0;
         add(lossCounter);
 
+        music = FlxG.sound.load(AssetHelper.music("breakfast"), 0, true);
+        music.play(false, FlxG.random.int(0, Math.floor(music.length * 0.5)));
+
         FlxTween.tween(background, {alpha: 0.6}, 0.4, {ease: FlxEase.quartInOut});
         FlxTween.tween(songText, {alpha: 1, y: 20}, 0.4, {ease: FlxEase.quartInOut, startDelay: 0.3});
         FlxTween.tween(lossCounter, {alpha: 1, y: 52}, 0.4, {ease: FlxEase.quartInOut, startDelay: 0.5});
-
-        music = FlxG.sound.load(AssetHelper.music("breakfast"), 0, true);
-        music.play(false, FlxG.random.int(0, Math.floor(music.length * 0.5)));
-        
-        changeSelection();
 
         #if ENGINE_SCRIPTING
         hxsCall("onCreatePost");
@@ -105,53 +103,28 @@ class PauseScreen extends MusicBeatSubState {
             music.volume += 0.01 * elapsed;
 
         if (controls.anyJustPressed(["up", "down"]))
-            changeSelection(controls.lastAction == "up" ? -1 : 1);
-        
-        #if ENGINE_SCRIPTING
-        if (controls.justPressed("accept") && list != null && !cancellableCall("onAccept")) {
-        #else
-        if (controls.justPressed("accept") && list != null) {
-        #end
-            switch (list[currentSelection].toLowerCase()) {
-                case "resume": 
-                    close();
-                case "restart song":
-                    TransitionSubState.skipNextTransIn = TransitionSubState.skipNextTransOut = true;
-                    AssetHelper.clearAssets = false;
-                    FlxG.resetState();
-                case "go to options":
-                    AssetHelper.clearAssets = false;
-                    FlxG.switchState(new OptionsMenu(true));
-                case "exit to menu":
-                    PlayState.lossCounter = 0;
-                    FlxG.switchState(switch (PlayState.gameMode) {
-                        case STORY: new StoryMenu();
-                        default: new FreeplayMenu();
-                    });
-            }
+            changeSelection((controls.lastAction == "up") ? -1 : 1);
 
-            #if ENGINE_SCRIPTING
-            hxsCall("onAcceptPost");
-            #end
-        }
+        if (controls.justPressed("accept"))
+            accept(currentList[currentSelection]);
 
         #if ENGINE_SCRIPTING
         hxsCall("onUpdatePost", [elapsed]);
         #end
     }
     
-    private function changeSelection(i:Int = 0):Void {
+    inline function changeSelection(i:Int = 0):Void {
         #if ENGINE_SCRIPTING
         if (cancellableCall("onSelectionChange", [i]))
             return;
         #end
 
-        currentSelection = FlxMath.wrap(currentSelection + i, 0, list.length - 1);
+        currentSelection = FlxMath.wrap(currentSelection + i, 0, currentList.length - 1);
         
         if (items != null) {
             for (i in items) {
                 i.target = items.members.indexOf(i) - currentSelection;
-                i.alpha = i.ID == currentSelection ? 1 : 0.6;
+                i.alpha = (i.ID == currentSelection) ? 1 : 0.6;
             }
         }
 
@@ -163,11 +136,67 @@ class PauseScreen extends MusicBeatSubState {
         #end
     }
 
+    inline function accept(item:String):Void {
+        #if ENGINE_SCRIPTING
+        if (cancellableCall("onAccept", [item]))
+            return;
+        #end
+
+        switch (item.toLowerCase()) {
+            case "resume": 
+                close();
+            case "restart song":
+                TransitionSubState.skipNextTransIn = TransitionSubState.skipNextTransOut = true;
+                AssetHelper.clearAssets = false;
+                FlxG.resetState();
+            case "go to options":
+                AssetHelper.clearAssets = false;
+                FlxG.switchState(new OptionsMenu(true));
+            case "exit to menu":
+                PlayState.lossCounter = 0;
+                FlxG.switchState(switch (PlayState.gameMode) {
+                    case STORY: new StoryMenu();
+                    default: new FreeplayMenu();
+                });
+        }
+
+        #if ENGINE_SCRIPTING
+        hxsCall("onAcceptPost", [item]);
+        #end
+    }
+
+    function changeList(list:String):Void {
+        currentList = itemLists[list];
+        currentSelection = 0;
+
+        while (items.length > 0)
+            items.remove(items.members[0], true).destroy();
+
+        for (i in 0...currentList.length) {
+            var item:Alphabet = items.recycle(Alphabet);
+            item.setPosition(0, 0);
+            item.text = currentList[i];
+            item.menuItem = true;
+
+            item.target = i;
+            item.ID = i;
+            item.doIntro();
+
+            items.remove(item);
+            items.add(item);
+        }
+
+        changeSelection();
+    }
+
     override function destroy():Void {
         if (music != null) {
             FlxG.sound.list.remove(music, true);
             music.destroy();
         }
+
+        currentList = null;
+        itemLists = null;
         
         super.destroy();
     }
