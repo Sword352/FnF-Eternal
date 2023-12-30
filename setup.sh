3c08a277893cbfcbc9ccedc51d1f839b66028fed
Sword352 <sword352email@gmail.com>
2023-12-30 11:51:00 +0100

    fix yaml library compiling issue

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ haxelib install flixel-addons
 haxelib git haxeui-core https://github.com/haxeui/haxeui-core
 haxelib git haxeui-flixel https://github.com/haxeui/haxeui-flixel
 haxelib install flixel-text-input
-haxelib git yaml https://github.com/swordcube/hx-yaml
+haxelib git yaml https://github.com/Sword352/hx-yaml
 haxelib git tjson https://github.com/Sword352/TJSON
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
 haxelib install hxvlc
