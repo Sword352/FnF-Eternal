5ec40f67dfcdb166b27fb7cadc1067f1cbe868fc
Sword352 <sword352email@gmail.com>
2024-03-16 16:23:52 +0100

    blammed lights (add week 3)

---
+++ mods/funkin/data/stages/philly.hx
@@ -0,0 +1,113 @@
+var lightColors:Array<FlxColor> = [0x30A2FC, 0x31FD8C, 0xFB32F4, 0xFD4430, 0xFAA733];
+var lightTiming:Float = 0;
+var lastLight:Int = -1;
+
+var trainEnabled:Bool = false;
+var trainMoving:Bool = false;
+var trainCooldown:Int = 0;
+var trainLoop:Int = 8;
+
+var trainFPS:Float = 1 / 24;
+var trainTimer:Float = 0;
+
+var trainSound:FlxSound;
+
+function onCreatePost():Void {
+    light.shader = new FlxRuntimeShader("
+        #pragma header
+        uniform float fadeMod;
+
+        float curve(float x) {
+            // custom easing, the fade gets faster on higher mod
+            return pow(x, 2. + 1.5 * x);
+        }
+        
+        void main() {
+            // gl_FragColor = clamp(flixel_texture2D(bitmap, openfl_TextureCoordv) - curve(fract(fadeMod)), 0.0, 1.0);
+            gl_FragColor = flixel_texture2D(bitmap, openfl_TextureCoordv) - curve(fadeMod);
+        }
+    ");
+    light.shader.setFloat("fadeMod", 1);
+
+    // Load the train sound effect
+    trainSound = FlxG.sound.load(Assets.sound("train_passes"));
+}
+
+function onUpdatePost(elapsed:Float):Void {
+    if (!music.playing) return;
+
+    light.shader.setFloat("fadeMod", ((Conductor.time - lightTiming) / (Conductor.crochet * Conductor.beatsPerMeasure)) * 1.25);
+
+    // 24 fps movement
+    if (trainEnabled) {
+        trainTimer += elapsed;
+        if (trainTimer >= trainFPS) {
+            updateTrain();
+            trainTimer = 0;
+        }
+    }
+}
+
+function onBeatHit(beat:Int):Void {
+    if (++trainCooldown > 8 && !trainEnabled && beat % 8 == 4 && FlxG.random.bool(30)) {
+        trainCooldown = FlxG.random.int(-4, 0);
+        startTrain();
+    }
+    else if (spectator?.animation.name == "hairFall" && spectator?.animation.finished)
+        spectator.forceDance(true);
+}
+
+function onMeasureHit():Void {
+    var color:Int = FlxG.random.int(0, lightColors.length - 1, [lastLight]);
+    light.color = lightColors[color];
+    lastLight = color;
+
+    lightTiming = Conductor.time;
+}
+
+function onSubStateOpened():Void {
+    if (trainEnabled)
+        trainSound.pause();
+}
+
+function onSubStateClosed():Void {
+    if (trainEnabled)
+        trainSound.resume();
+}
+
+function onGameOver():Void {
+    trainSound.stop();
+}
+
+function startTrain():Void {
+    trainEnabled = true;
+    trainSound.play(true);
+}
+
+function updateTrain():Void {
+    if (trainSound.time >= 4700 && !trainMoving) {
+        spectator?.playAnimation("hairBlow", true);
+        trainMoving = true;
+    }
+
+    if (trainMoving) {
+        var trainFinishing:Bool = (trainLoop <= 0);
+        train.x -= 400;
+
+        if (train.x < -2000 && !trainFinishing) {
+            trainFinishing = (--trainLoop <= 0);
+            train.x = -1150;
+        }
+
+        if (train.x < -4000 && trainFinishing)
+            resetTrain();
+    }
+}
+
+function resetTrain():Void {
+    spectator?.playAnimation("hairFall", true);
+    trainEnabled = trainMoving = false;
+
+    train.x = 2000;
+    trainLoop = 8;
+}
--- mods/funkin/data/stages/spooky.hx
+++ mods/funkin/data/stages/spooky.hx
@@ -1,5 +1,6 @@
 var lastBeat:Int = 0;
 var awaitBeat:Int = 8;
+var thunder:FlxSound;
 
 function onCreatePost():Void {
     // cache the sound effects
@@ -8,11 +9,12 @@ function onCreatePost():Void {
 }
 
 function onBeatHit(beat:Int):Void {
-    if (!FlxG.random.bool(10) || !music.playing || beat <= (lastBeat + awaitBeat))
-        return;
+    if (!FlxG.random.bool(10) || beat <= (lastBeat + awaitBeat)) return;
 
-    FlxG.sound.play(Assets.sound("thunder_" + FlxG.random.int(1, 2)));
     background.animation.play("lighting", true);
+
+    thunder = FlxG.sound.play(Assets.sound("thunder_" + FlxG.random.int(1, 2)));
+    thunder.onComplete = () -> thunder = null;
 
     for (character in [player, spectator]) {
         if (character.holdTime <= 0) {
@@ -23,4 +25,16 @@ function onBeatHit(beat:Int):Void {
 
     lastBeat = beat;
     awaitBeat = FlxG.random.int(8, 24);
+}
+
+function onSubStateOpened():Void {
+    thunder?.pause();
+}
+
+function onSubStateClosed():Void {
+    thunder?.resume();
+}
+
+function onGameOver():Void {
+    thunder?.stop();
 }
